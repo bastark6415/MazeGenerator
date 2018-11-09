6fcabd27577856d37dddfe0ea8a395f8d2dc2943
Kostiantyn Synytsia <bastark6415@gmail.com>
2018-11-09 23:27:11 +0200

    Added architecture with no implementation (only a few little functions)

---
+++ MazeGenerator/Generator/Generator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MazeGenerator.Types;
+
+namespace MazeGenerator.Generator
+{
+    public abstract class Generator
+    {
+		public int[,] mapMatrix { get; protected set; }
+		public Point start { get; protected set; }
+		public Point finish { get; protected set; }
+		public ushort height { get; protected set; }
+		public ushort width { get; protected set; }
+		public Generator(ushort height, ushort width)
+		{
+			this.width = width;
+			this.height = height;
+			mapMatrix = new int[height, width];
+		}
+		public static Generator Parse(string s);
+		public static bool TryParse(string s, out Generator result);
+		public void Action(bool showSteps, bool canDoNextStep);
+		public abstract void Generate(bool showSteps, bool canDoNextStep);
+		//To be continue...
+	}
+}
---
+++ MazeGenerator/Types/Path.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeGenerator.Types
+{
+    public class Path
+    {
+		public List<Point> path { get; private set; }
+		public Path()
+		{
+			path = new List<Point>();
+		}
+		public void AddPoint(Point p) => path.Add(p);
+		public void Clear() => path.Clear();
+		public void Reverse() => path.Reverse();
+    }
+}
