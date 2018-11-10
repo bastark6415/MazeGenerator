073110746b50b96cff364ce26ef47a8bd6eaa6f2
Kostiantyn Synytsia <bastark6415@gmail.com>
2018-11-11 01:27:58 +0200

    Startet coding algorithm

--- MazeGenerator/Generate/EllerAlgorithm.cs
+++ MazeGenerator/Generate/EllerAlgorithm.cs
@@ -11,7 +11,26 @@ namespace MazeGenerator.Generate
 		public EllerAlgorithm(ushort height, ushort width) : base(height, width) { }
 		public override void Generate(bool showSteps, ref bool canDoNextStep)
 		{
-			throw new NotImplementedException();
+			ushort[] currGeneration = new ushort[width];
+			bool[] usedSet = new bool[width];
+			Random random = new Random();
+			bool addWall;
+			for (int i = 0; i < height - 1; ++i)
+			{
+				for (int j = 0; j < currGeneration.Length; ++j)
+					if (currGeneration[j] == 0)
+						for (int k = 0; k < usedSet.Length; ++k)
+							if (!usedSet[k])
+							{
+								usedSet[k] = true;
+								currGeneration[j] = (ushort)(k + 1);
+							}
+				for (int j = 0; j < currGeneration.Length - 1; ++j)
+				{
+					addWall = random.Next(2) == 1;
+					mapMatrix[i, j].right = mapMatrix[i, j + 1].left
+				}
+			}
 		}
 	}
 }
