2e7e9ecc79f0ea6acbdc24b225a3069c7e7636c1
Kostiantyn Synytsia <bastark6415@gmail.com>
2018-11-11 12:28:16 +0200

    Startet Coding Eller's Generate

--- MazeGenerator/Generate/EllerAlgorithm.cs
+++ MazeGenerator/Generate/EllerAlgorithm.cs
@@ -11,7 +11,25 @@ namespace MazeGenerator.Generate
 		public EllerAlgorithm(ushort height, ushort width) : base(height, width) { }
 		public override void Generate(bool showSteps, ref bool canDoNextStep)
 		{
-			throw new NotImplementedException();
+			ushort[] currRow = new ushort[width];
+			bool[] usedSet = new bool[width + 1];
+			for (int i = 0; i < height - 1; ++i)
+			{
+				//assign for each cell unique set if it hasn't
+				for (int j = 0; j < width; ++j)
+					if (currRow[j] == 0)
+						for (ushort k = 1; k <= width; ++k)
+							if (!usedSet[k])
+							{
+								usedSet[k] = true;
+								currRow[j] = k;
+								break;
+							}
+				//create right walls
+				for (int j = 0; j < width; ++j) 
+				{
+				}
+			}
 		}
 	}
 }
