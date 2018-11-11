21a9d59c10a05ff9c41a0301ec53124a867a27e7
Kostiantyn Synytsia <bastark6415@gmail.com>
2018-11-11 02:03:09 +0200

    Implemented EllerAlgorithm

--- MazeGenerator/Generate/EllerAlgorithm.cs
+++ MazeGenerator/Generate/EllerAlgorithm.cs
@@ -25,10 +25,66 @@ namespace MazeGenerator.Generate
 								usedSet[k] = true;
 								currGeneration[j] = (ushort)(k + 1);
 							}
-				for (int j = 0; j < currGeneration.Length - 1; ++j)
+				for (int j = 1; j < currGeneration.Length; ++j)
 				{
+					if (currGeneration[j] == currGeneration[j - 1])
+						continue;
 					addWall = random.Next(2) == 1;
-					mapMatrix[i, j].right = mapMatrix[i, j + 1].left
+					mapMatrix[i, j].left = addWall;
+					if (!addWall)
+					{
+						usedSet[currGeneration[j] - 1] = false;
+						ushort setItem = currGeneration[j];
+						int k = j;
+						while (currGeneration[k] == setItem)
+						{
+							currGeneration[k] = currGeneration[j - 1];
+							++k;
+						}
+					}
+				}
+				for (int j = 0; j < currGeneration.Length; ++j)
+				{
+					int k = j + 1;
+					while (k < currGeneration.Length && currGeneration[k] == currGeneration[k - 1])
+						++k;
+					int walls = random.Next(1, k - j + 1);
+					for (int l = j; l < k; ++l)
+						mapMatrix[i + 1, l].up = true;
+					for (int l = 0; l < walls; ++l)
+					{
+						int pos = random.Next(j, k);
+						mapMatrix[i + 1, pos].up = false;
+					}
+					for (int l = j; l < k; ++l)
+						if (mapMatrix[i + 1, l].up == true)
+						{
+							usedSet[currGeneration[l] - 1] = false;
+							currGeneration[l] = 0;
+						}
+				}
+
+			}
+			for (int j = 0; j < currGeneration.Length; ++j)
+				if (currGeneration[j] == 0)
+					for (int k = 0; k < usedSet.Length; ++k)
+						if (!usedSet[k])
+						{
+							usedSet[k] = true;
+							currGeneration[j] = (ushort)(k + 1);
+						}
+			for (int j = 1; j < currGeneration.Length; ++j)
+			{
+				if (currGeneration[j] != currGeneration[j - 1])
+				{
+					ushort setItem = currGeneration[j];
+					int k = j;
+					mapMatrix[height - 1, j].left = false;
+					while (currGeneration[k] == setItem)
+					{
+						currGeneration[k] = currGeneration[j - 1];
+						++k;
+					}
 				}
 			}
 		}
