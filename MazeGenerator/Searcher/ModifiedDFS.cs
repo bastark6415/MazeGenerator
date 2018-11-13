2c6f49005c1b08ad20a1acd377e9541906dfe0ee
Kostiantyn Synytsia <bastark6415@gmail.com>
2018-11-13 18:40:50 +0200

    Added private variables deadblocksMap visited

--- MazeGenerator/Searcher/ModifiedDFS.cs
+++ MazeGenerator/Searcher/ModifiedDFS.cs
@@ -6,17 +6,20 @@ using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
 using MazeGenerator.Generate;
 using System.Windows.Media;
+using MazeGenerator.Types;
 
 namespace MazeGenerator.Searcher
 {
 	public class ModifiedDFS : Searcher
 	{
+		private bool[,] deadblocksMap;
+		private bool[,] visited;
 		public ModifiedDFS(Generator generator) : base(generator) { }
 		public override void Search(bool showSteps, ref bool canDoNextStep)
 		{
 			throw new NotImplementedException();
 		}
-		private void SetBlankAsDeadBlock(ref bool[,] deadblocksMap, ushort y, ushort x)
+		private void SetBlankAsDeadBlock(ushort y, ushort x)
 		{
 			if (y >= deadblocksMap.GetLength(0) || x >= deadblocksMap.GetLength(1))
 				return;
@@ -30,17 +33,17 @@ namespace MazeGenerator.Searcher
 			if (k >= 3)
 			{
 				deadblocksMap[y, x] = true;
-				if (!generator.mapMatrix[y, x].left)	SetBlankAsDeadBlock(ref deadblocksMap, y, (ushort)(x - 1));
-				if (!generator.mapMatrix[y, x].up)		SetBlankAsDeadBlock(ref deadblocksMap, (ushort)(y - 1), x);
-				if (!generator.mapMatrix[y, x].right)	SetBlankAsDeadBlock(ref deadblocksMap, y, (ushort)(x + 1));
-				if (!generator.mapMatrix[y, x].down)	SetBlankAsDeadBlock(ref deadblocksMap, (ushort)(y + 1), x);
+				if (!generator.mapMatrix[y, x].left)	SetBlankAsDeadBlock(y, (ushort)(x - 1));
+				if (!generator.mapMatrix[y, x].up)		SetBlankAsDeadBlock((ushort)(y - 1), x);
+				if (!generator.mapMatrix[y, x].right)	SetBlankAsDeadBlock(y, (ushort)(x + 1));
+				if (!generator.mapMatrix[y, x].down)	SetBlankAsDeadBlock((ushort)(y + 1), x);
 			}
 		}
-		private void SetDeadBlocks(ref bool[,] deadblocksMap)
+		private void SetDeadBlocks()
 		{
 			for (int i = 0; i < deadblocksMap.GetLength(0); ++i)
 				for (int j = 0; j < deadblocksMap.GetLength(1); ++j)
-					SetBlankAsDeadBlock(ref deadblocksMap, (ushort)i, (ushort)j);
+					SetBlankAsDeadBlock((ushort)i, (ushort)j);
 		}
 	}
 }
