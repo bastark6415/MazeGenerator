14622f0eedf2a3b484bbf4e2db446136583a22ef
Kostiantyn Synytsia <bastark6415@gmail.com>
2018-11-18 22:23:19 +0200

    Refactored BFS

--- MazeGenerator/Searchers/ModifiedBFS.cs
+++ MazeGenerator/Searchers/ModifiedBFS.cs
@@ -9,120 +9,84 @@ using MazeGenerator.Types;
 
 namespace MazeGenerator.Searchers
 {
-	/*class ModifiedBFS : Searcher
+	class ModifiedBFS : Searcher
 	{
-		private bool[,] deadBlocks;
 		public ModifiedBFS(Generator generator) : base(generator) { }
-		public override void Search(ref bool? canDoNextStep)
-		{
-			paths.Clear();
-			deadBlocks = new bool[generator.height, generator.width];
-			SetDeadBlocks();
-			Path startPath = new Path();
-			//DFS(ref canDoNextStep, generator.start, startPath);
-		}
-
 		protected override void SearchAsync(IProgress<string> progress, ManualResetEvent signal)
 		{
+			//Progress
+			progress?.Report("Started search");
+			signal?.Reset();
+			signal?.WaitOne();
+			//Search
 			paths.Clear();
-			deadBlocks = new bool[generator.height, generator.width];
-			SetDeadBlocks();
 			BFS(progress, signal);
+			//Progress
+			progress?.Report("Search has ended");
+			signal?.Dispose();
 		}
 		private void BFS(IProgress<string> progress, ManualResetEvent signal)
 		{
-			//Progress
-			progress?.Report($"Started search");
-			signal?.Reset();
-			signal?.WaitOne();
 			//Initialize path
-			Queue<Path> allPossiblePaths = new Queue<Path>();
+			LinkedList<Path> allPossiblePaths = new LinkedList<Path>();
 			Path tmp = new Path();
-			tmp.AddPoint(generator.start);
-			allPossiblePaths.Enqueue(tmp);
+			tmp.AddLast(start);
+			allPossiblePaths.AddLast(tmp);
 			//Search
 			while(allPossiblePaths.Count != 0)
 			{
-				tmp = allPossiblePaths.Dequeue();
-				Point lastPoint = tmp.path.Last();
+				tmp = allPossiblePaths.First.Value;
+				allPossiblePaths.RemoveFirst();
+				Point lastPoint = tmp.path.Last.Value;
 				if (lastPoint.x == generator.finish.x && lastPoint.y == generator.finish.y)
-				{
-					paths.Add(tmp);
-				}
+					paths.AddLast(tmp);
 				else
 				{
 					//left
-					if (!generator.mapMatrix[lastPoint.y, lastPoint.x].left && lastPoint.x > 0 &&
-						IsNotVisited(tmp, new Point { x = (int)(lastPoint.x - 1), y = lastPoint.y} ))
+					if (!mapMatrix[lastPoint.y, lastPoint.x].left && lastPoint.x > 0 &&
+						IsNotVisited(tmp, new Point (lastPoint.x - 1, lastPoint.y )))
 					{
 						Path p = new Path(tmp);
-						p.AddPoint(new Point { x = (int)(lastPoint.x - 1), y = lastPoint.y });
-						allPossiblePaths.Enqueue(p);
+						p.AddLast(new Point (lastPoint.x - 1, lastPoint.y ));
+						allPossiblePaths.AddLast(p);
 					}
 					//up
-					if (!generator.mapMatrix[lastPoint.y, lastPoint.x].up && lastPoint.y > 0 &&
-						IsNotVisited(tmp, new Point { x = lastPoint.x, y = (int)(lastPoint.y - 1) }))
+					if (!mapMatrix[lastPoint.y, lastPoint.x].up && lastPoint.y > 0 &&
+						IsNotVisited(tmp, new Point(lastPoint.x, lastPoint.y - 1)))
 					{
 						Path p = new Path(tmp);
-						p.AddPoint(new Point { x = lastPoint.x, y = (int)(lastPoint.y - 1) });
-						allPossiblePaths.Enqueue(p);
+						p.AddLast(new Point (lastPoint.x, lastPoint.y - 1));
+						allPossiblePaths.AddLast(p);
 					}
 					//right
-					if (!generator.mapMatrix[lastPoint.y, lastPoint.x].right && lastPoint.x < generator.width - 1 &&
-						IsNotVisited(tmp, new Point { x = (int)(lastPoint.x + 1), y = lastPoint.y }))
+					if (!mapMatrix[lastPoint.y, lastPoint.x].right && lastPoint.x < width - 1 &&
+						IsNotVisited(tmp, new Point (lastPoint.x + 1,lastPoint.y )))
 					{
 						Path p = new Path(tmp);
-						p.AddPoint(new Point { x = (int)(lastPoint.x + 1), y = lastPoint.y });
-						allPossiblePaths.Enqueue(p);
+						p.AddLast(new Point (lastPoint.x + 1, lastPoint.y));
+						allPossiblePaths.AddLast(p);
 					}
 					//down
-					if (!generator.mapMatrix[lastPoint.y, lastPoint.x].down && lastPoint.y < generator.height - 1 &&
-						IsNotVisited(tmp, new Point { x = lastPoint.x, y = (int)(lastPoint.y + 1) }))
+					if (!mapMatrix[lastPoint.y, lastPoint.x].down && lastPoint.y < height - 1 &&
+						IsNotVisited(tmp, new Point (lastPoint.x, lastPoint.y + 1)))
 					{
 						Path p = new Path(tmp);
-						p.AddPoint(new Point { x = lastPoint.x, y = (int)(lastPoint.y + 1) });
-						allPossiblePaths.Enqueue(p);
+						p.AddLast(new Point (lastPoint.x, lastPoint.y + 1));
+						allPossiblePaths.AddLast(p);
 					}
 					//Progress
-					List<Path> tmpPaths = paths;
-					if (signal != null) 
-						paths = allPossiblePaths.ToList();
-					progress?.Report($"Searching...");
-					signal?.Reset();
-					signal?.WaitOne();
-					paths = tmpPaths;
+					if (signal != null)
+					{
+						LinkedList<Path> tmpPaths = paths;
+						paths = allPossiblePaths;
+						progress?.Report("Searching...");
+						signal.Reset();
+						signal.WaitOne();
+						paths = tmpPaths;
+					}
 				}
 			}
-			//Progress
-			progress?.Report($"Search has ended");
-			signal?.Dispose();
 		}
-		private bool IsNotVisited(Path p, Point point) => !p.ContainsPoint(point) && !deadBlocks[point.y, point.x];
-		private void SetBlankAsDeadBlock(int y, int x)
-		{
-			if (y >= deadBlocks.GetLength(0) || x >= deadBlocks.GetLength(1))
-				return;
-			if (deadBlocks[y, x])
-				return;
-			int k = 0;
-			if (generator.mapMatrix[y, x].left || (x > 0 && deadBlocks[y, x - 1])) ++k;
-			if (generator.mapMatrix[y, x].up || (y > 0 && deadBlocks[y - 1, x])) ++k;
-			if (generator.mapMatrix[y, x].right || (x < generator.width - 1 && deadBlocks[y, x + 1])) ++k;
-			if (generator.mapMatrix[y, x].down || (y < generator.height - 1 && deadBlocks[y + 1, x])) ++k;
-			if (k >= 3)
-			{
-				deadBlocks[y, x] = true;
-				if (!generator.mapMatrix[y, x].left) SetBlankAsDeadBlock(y, (int)(x - 1));
-				if (!generator.mapMatrix[y, x].up) SetBlankAsDeadBlock((int)(y - 1), x);
-				if (!generator.mapMatrix[y, x].right) SetBlankAsDeadBlock(y, (int)(x + 1));
-				if (!generator.mapMatrix[y, x].down) SetBlankAsDeadBlock((int)(y + 1), x);
-			}
-		}
-		private void SetDeadBlocks()
-		{
-			for (int i = 0; i < deadBlocks.GetLength(0); ++i)
-				for (int j = 0; j < deadBlocks.GetLength(1); ++j)
-					SetBlankAsDeadBlock((int)i, (int)j);
-		}
-	}*/
+		private bool IsNotVisited(Path p, Point point) => !p.ContainsPoint(point) && !visited[point.y, point.x];
+	}
 }
