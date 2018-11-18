2fc7b85ae63526096e2f9bf43f7c7cb3b7b24198
Kostiantyn Synytsia <bastark6415@gmail.com>
2018-11-18 17:30:25 +0200

    Refactored Search

--- MazeGenerator/Searchers/Searcher.cs
+++ MazeGenerator/Searchers/Searcher.cs
@@ -13,20 +13,14 @@ namespace MazeGenerator.Searchers
 {
     public abstract class Searcher : GeneratorDecorator
     {
-		public List<Path> paths { get; protected set; }
+		public LinkedList<Path> paths { get; protected set; }
 		public Searcher(Generator generator) : base(generator)
 		{
-			paths = new List<Path>();
-		}
-		public override void Action(ref bool? canDoNextStep)
-		{
-			base.Action(ref canDoNextStep);
-			Search(ref canDoNextStep);
+			paths = new LinkedList<Path>();
 		}
 		public virtual Task Search(CancellationToken token, IProgress<string> progress, ManualResetEvent signal) =>
 			Task.Run(() => SearchAsync(progress, signal), token);
 		protected abstract void SearchAsync(IProgress<string> progress, ManualResetEvent signal);
-		public abstract void Search(ref bool? canDoNextStep);
 		public override BitmapSource ToBitmap(int wallPx, int cellPx)
 		{
 			bool[] allPaths = new bool[paths.Count];
@@ -43,24 +37,25 @@ namespace MazeGenerator.Searchers
 			int stride = (width * pf.BitsPerPixel + 7) / 8;
 			byte[] pixels = new byte[height * stride];
 			source.CopyPixels(pixels, stride, 0);
-			Color[] colors = new Color[] {Colors.Blue, Colors.Coral, Colors.Cyan,
-				Colors.Green, Colors.Red, Colors.Violet, Colors.Yellow, Colors.Orange, Colors.Pink,
+			Color[] colors = new Color[] {Colors.Blue, Colors.Yellow, Colors.Coral, Colors.Cyan,
+				Colors.Green, Colors.Red, Colors.Violet, Colors.Orange, Colors.Pink,
 				Colors.OrangeRed, Colors.Salmon, Colors.Tomato, Colors.Silver, Colors.PeachPuff,
 				Colors.Navy };
 			for (int i = 0; i < colors.Length; ++i)
-				colors[i].A = 180;
+				colors[i].A = 200;
 			for (int i = 0; i < pathsForShow.Length; ++i)
 			{
 				if (!pathsForShow[i])
 					continue;
-				foreach (Point p in paths[i].path)
+				foreach (Point p in paths.ElementAt(i).path)
 				{
 					for (int k = wallPx; k < wallPx + cellPx; ++k)
 					{
 						for (int l = wallPx; l < wallPx + cellPx; ++l)
 						{
 							Color prev = GetPixelColor(ref pixels, p.y, p.x, wallPx, cellPx, k, l, stride);
-							prev = Color.Add(prev, Color.Multiply(Color.Subtract(colors[i % colors.Length], prev), colors[i % colors.Length].ScA));
+							//prev = Color.Add(prev, Color.Multiply(Color.Subtract(colors[i % colors.Length], prev), colors[i % colors.Length].ScA));
+							prev = Color.Add(prev, Color.Multiply(colors[i % colors.Length], colors[i % colors.Length].A));
 							SetPixelColor(ref pixels, prev, p.y, p.x, wallPx, cellPx, k, l, stride);
 						}
 					}
