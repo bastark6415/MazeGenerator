899757a7e3e9ddb9eff01cef340d1b9bd1a36b3a
Kostiantyn Synytsia <bastark6415@gmail.com>
2018-11-20 04:37:29 +0200

    Added documentation to Path and Point

--- MazeGenerator/Types/Path.cs
+++ MazeGenerator/Types/Path.cs
@@ -6,20 +6,75 @@ using System.Threading.Tasks;
 
 namespace MazeGenerator.Types
 {
+	/// <summary>
+	/// Class <c>Path</c> is the path in maze.
+	/// </summary>
+	/// <remarks>
+	/// Class contains a LinkedList of <c>Point</c> inside.
+	/// Points one by one means path in maze.
+	/// We can add or remove last point.
+	/// Either we can check if the path contains <c>Point</c>.
+	/// Or we can clear path (delete all points).
+	/// </remarks>
     public class Path
     {
+		/// <value>
+		///	 <c>Linked List</c> contains points of path inside.
+		/// </value>
 		public LinkedList<Point> path { get; private set; }
+		/// <summary>
+		/// Constructor of <c>Path</c>.
+		/// </summary>
+		/// <remarks>
+		/// Creates new LinkedList.
+		/// </remarks>
 		public Path()
 		{
 			path = new LinkedList<Point>();
 		}
+		/// <summary>
+		/// Copy constructor of <c>Path</c>.
+		/// </summary>
+		/// <remarks>
+		/// Creates new path from <paramref name="p"/> path.
+		/// </remarks>
+		/// <param name="p">An another path.</param>
+		/// <exception cref="System.ArgumentNullException">Throws if <paramref name="p"/> is null.</exception>
 		public Path(Path p)
 		{
-			path = new LinkedList<Point>(p.path);
+			path = new LinkedList<Point>(p?.path);
 		}
+		/// <summary>
+		/// Adds new point to path.
+		/// </summary>
+		/// <remarks>
+		/// Adds new point <paramref name="p"/> in the end of path.
+		///	</remarks>
+		/// <param name="p">Point that needed to add.</param>
 		public void AddLast(Point p) => path.AddLast(p);
+		/// <summary>
+		/// Removes last point from path.
+		/// </summary>
+		/// <remarks>
+		///	Removes the last point from the path.
+		///	</remarks>
+		///	<exception cref="System.ArgumentNullException"></exception>
 		public void RemoveLast() => path.RemoveLast();
+		/// <summary>
+		/// Check if point <paramref name="p"/> belongs to path.
+		/// </summary>
+		/// <remarks>
+		/// <paramref name="p"/> compared for each point in.
+		/// </remarks>
+		/// <param name="p">Point that should be search.</param>
+		/// <returns>Return true if path contains<paramref name="p"/> </returns>
 		public bool ContainsPoint(Point p) => path.Contains(p);
+		/// <summary>
+		/// Removes all points from path
+		/// </summary>
+		/// <remarks>
+		/// Clean paths
+		/// </remarks>
 		public void Clear() => path.Clear();
     }
 }
--- MazeGenerator/Types/Point.cs
+++ MazeGenerator/Types/Point.cs
@@ -6,11 +6,34 @@ using System.Threading.Tasks;
 
 namespace MazeGenerator.Types
 {
+	/// <summary>
+	/// Class <c>Point</c> is a point xy in maze"
+	/// </summary>
+	/// <remarks>
+	///	Creates a new <c>Point</c> structure
+	/// </remarks>
     public struct Point
     {
 		public int x, y;
+		/// <summary>
+		///   Create a new <c>Point</c> from coordinates (<paramref name="x", <paramref name="y">;
+		/// </summary>
+		/// <param name="x">An int Value</param>
+		/// <param name="y">An int Value</param>
 		public Point(int x, int y) { this.x = x; this.y = y; }
+		/// <summary>
+		/// Comparison of two point
+		/// </summary>
+		/// <param name="p1">An integer point</param>
+		/// <param name="p2">An integer point</param>
+		/// <returns>True if coordinates are equal, false another</returns>
 		public static bool operator ==(Point p1, Point p2) => p1.x == p2.x && p1.y == p2.y;
+		/// <summary>
+		/// Comparison of two points
+		/// </summary>
+		/// <param name="p1">An integer</param>
+		/// <param name="p2">An integer</param>
+		/// <returns>Fale if coordinates are equal, true anoter</returns>
 		public static bool operator !=(Point p1, Point p2) => !(p1 == p2);
     }
 }
