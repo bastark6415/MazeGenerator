d9656fda840a3d4faa6732c9bc437abb57d856bd
Kostiantyn Synytsia <bastark6415@gmail.com>
2018-11-20 03:44:31 +0200

    Added doc for Cell

--- MazeGenerator/Types/Cell.cs
+++ MazeGenerator/Types/Cell.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace MazeGenerator.Types
 {
+	/// <summary>
+	/// The main item of <c>Generator</c> class
+	/// </summary>
+	/// <remarks>
+	/// Contain four boolean variables for each wall(left, up, down, right)
+	/// If variable is true, it is mean that on this side <c>Cell</c> have wall.
+	/// </remarks>
 	public struct Cell
 	{
 		public bool left;
